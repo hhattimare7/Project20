0be59f613ba833da0beed9b4a1e4f7dc1d27508e
hhattimare7 <77083697+hhattimare7@users.noreply.github.com>
2021-01-16 16:37:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var tom, jerry;
+var tomI, jerryI;
+
+function preload() {
+    tomI = loadImage("tomOne.png");
+    jerryI = loadImage("jerryOne.png");
+}
+
+function setup(){
+    createCanvas(1000,800);
+    
+
+}
+
+function draw() {
+
+    background("garden.png");
+    //Write condition here to evalute if tom and jerry collide
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+
+
+}
